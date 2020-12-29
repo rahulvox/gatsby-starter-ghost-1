0703eaabd523ba42a860e13d560a7a422ca811f4
rahul dadsena <rahuldadsena19@gmail.com>
2020-12-29 15:33:25 +0530

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -9,7 +9,7 @@ module.exports = {
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Dadsena Community`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `Dadsena`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
